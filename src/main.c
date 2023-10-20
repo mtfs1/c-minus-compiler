f57bfa1ea6b7fe1d9e3a030302431bcc2c143b93
mtfs1 <math.santos.2002@gmail.com>
2023-10-19 21:59:56 -0300

    feat: parse arguments and open source code file

--- src/main.c
+++ src/main.c
@@ -1,4 +1,24 @@
-int main() {
+#include <stdio.h>
+
+#include "buffered_read.h"
+
+
+int main(int argc, char **argv) {
+  if(argc != 2) {
+    printf("ERROR: Wrong number of args\n");
+    printf("Correct usage:\n");
+    printf("  ./compiler c_minus_source_file\n");
+    return -1;
+  }
+
+  FILE *fp;
+  if((fp = fopen(argv[1], "r")) == NULL) {
+    printf("ERROR: Wrong file path\n");
+    return -1;
+  }
+
+  struct Buffer file_buffer = allocate_buffer(fp);
+
   return 0;
 }
 
